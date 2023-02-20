751854a45286b803ebd075182eec1dad0f60accb
BigCee12 <reginaldejikeme852@gmail.com>
2023-02-20 01:13:35 +0100

    fixed requirements.txt

--- ifitness/settings.py
+++ ifitness/settings.py
@@ -14,9 +14,7 @@ import os
 from decouple import config
 
 
-env = environ.Env()
 
-environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
